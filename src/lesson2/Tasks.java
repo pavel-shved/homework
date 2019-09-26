f1987cfc4358a58284c98673bcb052a8ac20ab2c
pavel-shved <pavelshved1990@gmail.com>
2019-09-26 09:40:25 +0300

    Обновлен файл .gitignore

--- src/lesson2/Tasks.java
+++ src/lesson2/Tasks.java
@@ -1,4 +1,67 @@
 package lesson2;
 
 public class Tasks {
+    public static void main(String[] args) {
+        System.out.println("Tasks");
+        //Task 1. Write a Java program to sort a numeric array and a string array.
+//        Задача 1. Напишите Java-программу для сортировки числового массива и строкового массива.
+
+        //Task 2. Write a Java program to sum values of an array.
+//        Задача 2. Напишите Java-программу для суммирования значений массива.
+
+        //Task 3. Write a Java program to print the following grid
+
+        //Task 4.  Write a Java program to calculate the average value of array elements.
+//        Задача 4. Напишите Java-программу для расчета среднего значения элементов массива.
+
+        //Task 5.  Write a Java program to test if an array contains a specific value.
+//        Задача 5. Напишите Java-программу для проверки, содержит ли массив конкретное значение.
+
+        //Task 6.  Write a Java program to find the index of an array element.
+//        Задача 6. Напишите программу на Java, чтобы найти индекс элемента массива.
+
+        //Task 7.  Write a Java program to remove a specific element from an array.
+//        Задача 7. Напишите Java-программу для удаления определенного элемента из массива.
+
+        //Task 8.  Write a Java program to copy an array by iterating the array.
+//        Задача 8. Напишите Java-программу для копирования массива путем итерации массива.
+
+        //Task 9.  Write a Java program to insert an element (specific position) into an array.
+//        Задача 9. Напишите Java-программу для вставки элемента (определенной позиции) в массив.
+
+        //Task 10. Write a Java program to find the maximum and minimum value of an array.
+//        Задача 10. Напишите программу на Java, чтобы найти максимальное и минимальное значение массива.
+
+        //Task 11. Write a Java program to reverse an array of integer values.
+//        Задача 11. Напишите Java-программу для обращения к массиву целочисленных значений.
+
+        //Task 12. Write a Java program to find the duplicate values of an array of integer values.
+//        Задача 12. Напишите программу на Java, чтобы найти повторяющиеся значения в массиве целочисленных значений.
+
+        //Task 13. Write a Java program to find the duplicate values of an array of string values.
+//        Задача 13. Написать Java-программу для поиска дублирующихся значений массива строковых значений.
+
+        //Task 14. Write a Java program to find the common elements between two arrays (string values).
+//        Задача 14. Напишите программу на Java, чтобы найти общие элементы между двумя массивами (строковые значения).
+
+        //Task 15. Write a Java program to find the common elements between two arrays of integers.
+//        Задача 15. Напишите программу на Java, чтобы найти общие элементы между двумя массивами целых чисел.
+
+        //Task 16. Write a Java program to remove duplicate elements from an array.
+//        Задача 16. Напишите Java-программу для удаления дублирующихся элементов из массива.
+
+        //Task 17. Write a Java program to find the second largest element in an array.
+//        Задача 17. Написать программу на Java, чтобы найти второй по величине элемент в массиве.
+
+        //Task 18. Write a Java program to find the second smallest element in an array.
+//        Задача 18. Напишите программу на Java, чтобы найти второй наименьший элемент в массиве.
+
+        //Task 19. Write a Java program to add two matrices of the same size.
+//        Задача 19. Напишите Java-программу для добавления двух матриц одинакового размера.
+
+        //Task 20. Write a Java program to convert an array to ArrayList.
+//        Задача 20. Напишите Java-программу для преобразования массива в ArrayList.
+
+
+    }
 }
