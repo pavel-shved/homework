3d6f8854ae7e787e79329546243bb483e2cd4999
pavel-shved <pavelshved1990@gmail.com>
2019-09-27 16:57:37 +0300

    Сделал домашку №2.

--- src/lesson2/TasksOfLessonTwo.java
+++ src/lesson2/TasksOfLessonTwo.java
@@ -1,5 +1,8 @@
 package lesson2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TasksOfLessonTwo {
     public static void main(String[] args) {
         System.out.println("TasksOfLessonTwo");
@@ -12,16 +15,37 @@ public class TasksOfLessonTwo {
         runTask8();
         runTask9();
         runTask10();
-//        runTask11();
-//        runTask12();
-//        runTask13();
-//        runTask14();
-//        runTask15();
-//        runTask16();
-//        runTask17();
-//        runTask18();
+        runTask11();
+        runTask12();
+        runTask13();
+        runTask14();
+        runTask15();
+        runTask16();
+        runTask17();
+        runTask18();
 //        runTask19();
-//        runTask20();
+        runTask20();
+    }
+
+    private static void printArray(double[] array) {
+        for (double d : array) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printArray(String[] array) {
+        for (String s : array) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
 
 
@@ -30,10 +54,7 @@ public class TasksOfLessonTwo {
     private static void runTask1() {
         double[] numArray = new double[]{1, 4, -3, 5};
         System.out.print("1) Числовой массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         double temp;
         for (int i = 0; i < numArray.length; i++) {
@@ -47,17 +68,11 @@ public class TasksOfLessonTwo {
         }
 
         System.out.print("Числовой массив на выходе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         String[] stringArray = new String[]{"баян", "банан", "ананас", "картофель"};
         System.out.print("Строковый массив на входе: ");
-        for (String v : stringArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(stringArray);
 
         String tempStr;
         for (int i = 0; i < stringArray.length; i++) {
@@ -70,10 +85,7 @@ public class TasksOfLessonTwo {
             }
         }
         System.out.print("Строковый массив на выходе: ");
-        for (String v : stringArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(stringArray);
     }
 
 
@@ -83,11 +95,10 @@ public class TasksOfLessonTwo {
         double[] numArray = new double[]{1, 4, -3, 5};
         double sumArray = 0;
         System.out.print("2) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-            sumArray = sumArray + v;
+        printArray(numArray);
+        for (double d : numArray) {
+            sumArray = sumArray + d;
         }
-        System.out.println();
         System.out.println("Сумма элементов массива: " + sumArray);
     }
 
@@ -98,11 +109,10 @@ public class TasksOfLessonTwo {
         double[] numArray = new double[]{1, 4, -3, 6};
         double sumArray = 0;
         System.out.print("4) Массив на входе: ");
+        printArray(numArray);
         for (double v : numArray) {
-            System.out.print(v + " ");
             sumArray = sumArray + v;
         }
-        System.out.println();
         System.out.println("Среднее значение элементов массива: " + (sumArray / numArray.length));
     }
 
@@ -111,10 +121,8 @@ public class TasksOfLessonTwo {
     private static void runTask5() {
         double[] numArray = new double[]{1, 4, -3, 6};
         System.out.print("5) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
+
         double findValue = 6;
         System.out.print("Содержит ли массив елемент со значением " + findValue + "? Ответ: ");
         String answer = "";
@@ -136,10 +144,8 @@ public class TasksOfLessonTwo {
     private static void runTask6() {
         double[] numArray = new double[]{1, 4, -3, 6};
         System.out.print("6) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
+
         double findIndexOfValue = 4;
         System.out.print("Какой индекс у элемента со значением " + findIndexOfValue + "? Ответ: ");
         int i;
@@ -158,10 +164,7 @@ public class TasksOfLessonTwo {
     private static void runTask7() {
         double[] numArray = new double[]{1, 4, -3, 6, 4};
         System.out.print("7) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         double findValue = 4;
         int countDelElements = 0;
@@ -183,10 +186,7 @@ public class TasksOfLessonTwo {
             }
             numArray = tempArray;
             System.out.print("Массив на выходе: ");
-            for (double v : numArray) {
-                System.out.print(v + " ");
-            }
-            System.out.println();
+            printArray(numArray);
         } else {
             System.out.println("В массиве нет элементов со значением " + findValue + "...");
         }
@@ -198,20 +198,14 @@ public class TasksOfLessonTwo {
         double[] numArray = new double[]{1, 4, -3, 6, 4};
         double[] copyNumArray = new double[numArray.length];
         System.out.print("8) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         for (int i = 0; i < numArray.length; i++) {
             copyNumArray[i] = numArray[i];
         }
 
         System.out.print("Скопированный массив на выходе: ");
-        for (double v : copyNumArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(copyNumArray);
 
     }
 
@@ -220,32 +214,26 @@ public class TasksOfLessonTwo {
     private static void runTask9() {
         double[] numArray = new double[]{1, 4, -3, 6, 4};
         System.out.print("9) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         int position = 3;
         double value = 5.5;
-        System.out.println("Вставляем элемент со значением "+value+" в позицию "+position);
+        System.out.println("Вставляем элемент со значением " + value + " в позицию " + position);
 
-        double[] tempArray = new double[numArray.length+1];
+        double[] tempArray = new double[numArray.length + 1];
         for (int i = 0; i < tempArray.length; i++) {
-            if (i < position){
+            if (i < position) {
                 tempArray[i] = numArray[i];
-            } else if (i == position){
+            } else if (i == position) {
                 tempArray[i] = value;
-            } else{
-                tempArray[i] = numArray[i-1];
+            } else {
+                tempArray[i] = numArray[i - 1];
             }
         }
 
         numArray = tempArray;
         System.out.print("Массив на выходе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
     }
 
@@ -254,54 +242,270 @@ public class TasksOfLessonTwo {
     private static void runTask10() {
         double[] numArray = new double[]{1, 4, -3, 6, 4};
         System.out.print("10) Массив на входе: ");
-        for (double v : numArray) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
+        printArray(numArray);
 
         double maxValue = numArray[0];
         double minValue = numArray[0];
         for (double v : numArray) {
-            if (v < maxValue){
+            if (v < maxValue) {
                 maxValue = v;
             }
-            if (v > minValue){
+            if (v > minValue) {
                 minValue = v;
             }
         }
-        System.out.println("Минимальное значение массива: "+minValue);
-        System.out.println("Максимальное значение массива: "+maxValue);
+        System.out.println("Минимальное значение массива: " + minValue);
+        System.out.println("Максимальное значение массива: " + maxValue);
     }
 
     //Task 11. Write a Java program to reverse an array of integer values.
 //        Задача 11. Напишите Java-программу для обращения к массиву целочисленных значений.
+    private static void runTask11() {
+        int[] numArray = new int[]{1, 4, -3, 6, 4};
+        System.out.print("11) Массив на входе: ");
+        printArray(numArray);
+
+        int j = 0;
+        int[] tempArray = new int[numArray.length];
+        for (int i = tempArray.length - 1; i >= 0; i--) {
+            tempArray[j] = numArray[i];
+            j++;
+        }
+        numArray = tempArray;
+        System.out.print("Перевернутый массив на выходе: ");
+        printArray(numArray);
+    }
 
     //Task 12. Write a Java program to find the duplicate values of an array of integer values.
 //        Задача 12. Напишите программу на Java, чтобы найти повторяющиеся значения в массиве целочисленных значений.
+    private static void runTask12() {
+        int[] numArray = new int[]{1, 4, -3, 6, 6, 6, 6, 4};
+        System.out.print("12) Массив на входе: ");
+        printArray(numArray);
+
+        int[] iteratorsArray = new int[numArray.length];
+        int tempIterator = 0;
+
+        for (int i = 0; i < numArray.length; i++) {
+            for (int j = i + 1; j < numArray.length; j++) {
+                if (numArray[i] == numArray[j]) {
+                    iteratorsArray[tempIterator] = i;
+                    tempIterator++;
+                    break;
+                }
+            }
+        }
+
+        System.out.print("Повторяющиеся значения в массиве: ");
+        for (int i = 0; i < tempIterator; i++) {
+            System.out.print(numArray[iteratorsArray[i]] + " ");
+        }
+        System.out.println();
+    }
 
     //Task 13. Write a Java program to find the duplicate values of an array of string values.
 //        Задача 13. Написать Java-программу для поиска дублирующихся значений массива строковых значений.
+    private static void runTask13() {
+        String[] stringArray = new String[]{"Java", "Piton", "Java", "Kotlin", "Java"};
+        System.out.print("13) Массив на входе: ");
+        printArray(stringArray);
+
+        int[] iteratorsArray = new int[stringArray.length];
+        int tempIterator = 0;
+
+        for (int i = 0; i < stringArray.length; i++) {
+            for (int j = i + 1; j < stringArray.length; j++) {
+                if (stringArray[i].equals(stringArray[j])) {
+                    iteratorsArray[tempIterator] = i;
+                    tempIterator++;
+                    break;
+                }
+            }
+        }
+
+        if (tempIterator > 0) {
+            System.out.print("Повторяющиеся значения в массиве: ");
+            for (int i = 0; i < tempIterator; i++) {
+                System.out.print(stringArray[iteratorsArray[i]] + " ");
+            }
+            System.out.println();
+        } else System.out.println("Повторяющихся значений в массиве нет!");
+
+    }
 
     //Task 14. Write a Java program to find the common elements between two arrays (string values).
 //        Задача 14. Напишите программу на Java, чтобы найти общие элементы между двумя массивами (строковые значения).
+    private static void runTask14() {
+        String[] stringFirstArray = new String[]{"Java", "Piton", "Kotlin"};
+        String[] stringSecondArray = new String[]{"Банан", "Java", "Баян", "Султан"};
+
+        System.out.print("14) Первый массив: ");
+        printArray(stringFirstArray);
+        System.out.print("Второй массив: ");
+        printArray(stringSecondArray);
+
+        int[] iteratorsArray = new int[Math.max(stringFirstArray.length, stringSecondArray.length)];
+        int tempIterator = 0;
+
+        for (int i = 0; i < stringFirstArray.length; i++) {
+            for (String s : stringSecondArray) {
+                if (stringFirstArray[i].equals(s)) {
+                    iteratorsArray[tempIterator] = i;
+                    tempIterator++;
+                    break;
+                }
+            }
+        }
+
+        if (tempIterator > 0) {
+            System.out.print("Общие значения в массивах: ");
+            for (int i = 0; i < tempIterator; i++) {
+                System.out.print(stringFirstArray[iteratorsArray[i]] + " ");
+            }
+            System.out.println();
+        } else System.out.println("Общих значений в массивах нет!");
+    }
 
     //Task 15. Write a Java program to find the common elements between two arrays of integers.
 //        Задача 15. Напишите программу на Java, чтобы найти общие элементы между двумя массивами целых чисел.
+    private static void runTask15() {
+        int[] intFirstArray = new int[]{1, 4, -3, 6};
+        int[] intSecondArray = new int[]{6, 0, -5, 0, 4};
+
+        System.out.print("15) Первый массив: ");
+        printArray(intFirstArray);
+        System.out.print("Второй массив: ");
+        printArray(intSecondArray);
+
+        int[] iteratorsArray = new int[Math.max(intFirstArray.length, intSecondArray.length)];
+        int tempIterator = 0;
+
+        for (int i = 0; i < intFirstArray.length; i++) {
+            for (int j : intSecondArray) {
+                if (intFirstArray[i] == j) {
+                    iteratorsArray[tempIterator] = i;
+                    tempIterator++;
+                    break;
+                }
+            }
+        }
+
+        if (tempIterator > 0) {
+            System.out.print("Общие значения в массивах: ");
+            for (int i = 0; i < tempIterator; i++) {
+                System.out.print(intFirstArray[iteratorsArray[i]] + " ");
+            }
+            System.out.println();
+        } else System.out.println("Общих значений в массивах нет!");
+    }
 
     //Task 16. Write a Java program to remove duplicate elements from an array.
 //        Задача 16. Напишите Java-программу для удаления дублирующихся элементов из массива.
+    private static void runTask16() {
+        int[] numArray = new int[]{1, 4, -3, 6, 4, 7, 6, 4};
+        System.out.print("16) Массив на входе: ");
+        printArray(numArray);
+        Arrays.sort(numArray);
+
+        int tempArrayLength = 1;
+        for (int i = 1; i < numArray.length; i++) {
+            if (numArray[i] != numArray[i - 1]) {
+                tempArrayLength++;
+            }
+        }
+
+        int[] tempArray = new int[tempArrayLength];
+        tempArray[0] = numArray[0];
+        int j = 1;
+        for (int i = 1; i < numArray.length; i++) {
+            if (numArray[i] != numArray[i - 1]) {
+                tempArray[j++] = numArray[i];
+            }
+        }
+        numArray = tempArray;
+        System.out.print("Массив без дубликатов: ");
+        printArray(numArray);
+    }
 
     //Task 17. Write a Java program to find the second largest element in an array.
 //        Задача 17. Написать программу на Java, чтобы найти второй по величине элемент в массиве.
+    private static void runTask17() {
+        int[] numArray = new int[]{1, 4, -3, 6, 5, 7, 12};
+        System.out.print("17) Массив на входе: ");
+        printArray(numArray);
+
+        int maxElement = numArray[0];
+        int minElement = numArray[0];
+        for (int i : numArray) {
+            if (i > maxElement) {
+                maxElement = i;
+            } else if (i < minElement) {
+                minElement = i;
+            }
+        }
+
+        int secondMaxElement = minElement;
+        for (int i : numArray) {
+            if (i == maxElement) {
+                break;
+            } else if (i > secondMaxElement) {
+                secondMaxElement = i;
+            }
+        }
+
+        System.out.println("Второй по величине элемент: " + secondMaxElement);
+    }
 
     //Task 18. Write a Java program to find the second smallest element in an array.
 //        Задача 18. Напишите программу на Java, чтобы найти второй наименьший элемент в массиве.
+    private static void runTask18() {
+        int[] numArray = new int[]{1, 4, -3, 6, 5, 7, 12};
+        System.out.print("18) Массив на входе: ");
+        printArray(numArray);
+
+        int maxElement = numArray[0];
+        int minElement = numArray[0];
+        for (int i : numArray) {
+            if (i > maxElement) {
+                maxElement = i;
+            } else if (i < minElement) {
+                minElement = i;
+            }
+        }
+
+        int secondMinElement = maxElement;
+        for (int i : numArray) {
+            if (i == minElement) {
+                break;
+            } else if (i < secondMinElement) {
+                secondMinElement = i;
+            }
+        }
+
+        System.out.println("Второй наименьший элемент: " + secondMinElement);
+    }
 
     //Task 19. Write a Java program to add two matrices of the same size.
 //        Задача 19. Напишите Java-программу для добавления двух матриц одинакового размера.
 
+
     //Task 20. Write a Java program to convert an array to ArrayList.
 //        Задача 20. Напишите Java-программу для преобразования массива в ArrayList.
+    private static void runTask20() {
+        int[] numArray = new int[]{1, 4, -3, 6, 5, 7, 12};
+        System.out.print("20) Массив на входе: ");
+        printArray(numArray);
 
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i : numArray) {
+            arrayList.add(i);
+        }
+
+        System.out.println("ArrayList на выходе :");
+        for (Integer integer : arrayList) {
+            System.out.print(integer + " ");
+        }
+
+    }
 
 }
